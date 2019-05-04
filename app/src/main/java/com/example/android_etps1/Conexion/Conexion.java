f6101a5802ff3a3c5f18f041e420c68894491e1e
Douglas Guzman <43095613+DouglasG96@users.noreply.github.com>
2019-05-04 16:01:42 -0600

    Prueba de Conexion con firebase
    Se Descargo la libreria y se sincronizo con el proyecto creado en firebase

---
+++ app/src/main/java/com/example/android_etps1/Conexion/Conexion.java
@@ -0,0 +1,12 @@
+package com.example.android_etps1.Conexion;
+
+import com.google.firebase.FirebaseApp;
+
+public class Conexion {
+    Conexion()
+    {
+        FirebaseApp objConexion;
+    }
+
+
+}
